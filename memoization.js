ddf46a83f6476b19d999df9be22177fb6f3822d3
AnnaAlexandrovna <rassvetnavolge1>
2021-01-03 23:12:00 +0300

    add multi argument memoization

--- memoization.js
+++ memoization.js
@@ -1,3 +1,4 @@
+//мемоизация с двумя аргументами
 class calcPow {
     funCache = {};
     pow(x, n)   {
@@ -20,3 +21,25 @@ class calcPow {
 let p = new calcPow();
 console.log(p.pow(44,5)); // вычислено
 console.log(p.pow(44,3)); // взято из кеша
+
+//мультиаргументная мемоизация
+class calcSum {
+    funCache = {};
+    sum(...args) {
+        if (`${args.toString()}` in this.funCache) {
+            console.log('Взято из кеша');
+            return this.funCache[`${args.toString()}`];
+        } else {
+            console.log('Вычислено');
+            let res = 0;
+            for(let i=0; i<args.length; i++){
+                res+=args[i];
+            }
+            this.funCache[`${args.toString()}`] = res;
+            return res;
+        }
+    }
+}
+let s = new calcSum();
+console.log(s.sum(1,4,5));//вычислено
+console.log(s.sum(1,4,5));//взято из кеша
